886fc148c3d2260aca763f59c36527237e418d03
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-04-05 11:27:58 -0400

    FIX params in new page

--- controllers/ticket.js
+++ controllers/ticket.js
@@ -6,16 +6,18 @@ module.exports = {
     create
 }
 
- async function newTicket(req, res) {
-    const flight = Flight.findById(req.params.id);
-    console.log(flight);
-    res.render('ticket/new', {title: 'Purchase Ticket'}, flight);
+async function newTicket(req, res) {
+    console.log('id--->',req.params.id)
+    const flight = await Flight.findById(req.params.id);
+    res.render('ticket/new', {title: 'Purchase Ticket', flight});
+    // console.log('flight -->',flight, 'number-->', flight.flightNumber);
 }
 
 async function create(req, res) {
     try {
-        const ticket = Ticket.find({flight: flight._id}).populate('flight');
-        res.redirect(`/flights/${flight._id}`)
+        const flight = await Flight.findById(req.params.id);
+        const ticket = await Ticket.find({flight: flight._id}).populate('flight');
+        res.redirect(`/flights/${flight._id}`, flight, ticket)
     } catch(error) {
         console.log('error: ', error);
         res.render('error', { error: error})
